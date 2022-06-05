64a0fe1f224fcad8b35eb23db1e551e545940a23
JhonatanD09 <joranada@gmail.com>
2022-06-05 09:57:36 -0500

    update-priority-queue

--- src/models/Queue.java
+++ src/models/Queue.java
@@ -12,60 +12,63 @@ public class Queue<T> {
 		Node<T> temp = new Node<T>(data, priority);
 		if (firstNode == null) {
 			firstNode = temp;
-		}else if(firstNode.getPriority() > priority ){
+		} else if (firstNode.getPriority() > priority) {
 			Node<T> aux = firstNode;
 			firstNode = temp;
 			firstNode.setNext(aux);
-		}else{
+		} else {
 			pushPriority(temp);
 		}
 
 	}
 
-	
+	public void show() {
+		Node<T> temp = firstNode;
+		while (temp != null) {
+			System.out.println(temp.getData() + " " + temp.getPriority());
+			temp = temp.getNext();
+		}
+
+	}
+
 	private void pushPriority(Node<T> temp) {
 		Node<T> tempNode = firstNode;
-		while (tempNode != null  ) {
-				tempNode = validePushByPriority(temp, tempNode);
+		while (tempNode != null ) {
+			System.out.println(tempNode.getPriority()+" -- "+ temp.getPriority());
+			if(tempNode.getPriority() > temp.getPriority()) {
+				temp.setNext(tempNode);
+				searchDad(tempNode).setNext(temp);
+				tempNode = null;
+			}else if(tempNode.getNext()==null) {
+					tempNode.setNext(temp);
+					tempNode = null;
+			}else {
+				tempNode = tempNode.getNext();				
+			}
 		}
 	}
 
-	private Node<T> validePushByPriority(Node<T> temp, Node<T> tempNode) {
-		if ( tempNode.getNext().getPriority()> temp.getPriority()) {
-			tempNode = pushByPriorty(temp, tempNode);
-		}else {
-			tempNode = valideFinishQueueProprity(temp, tempNode);
+	private Node<T> searchDad(Node<T> node) {
+		Node<T> tempNode = firstNode;
+		while (tempNode != null) {
+			if (tempNode.getNext().equals(node)) {
+				return tempNode;
+			}
+			tempNode = tempNode.getNext();
 		}
-		return tempNode;
+		return null;
 	}
 
-	private Node<T> pushByPriorty(Node<T> temp, Node<T> tempNode) {
-		temp.setNext(tempNode.getNext());
-		tempNode.setNext(temp);
-		tempNode = null;
-		return tempNode;
-	}
-
-	private Node<T> valideFinishQueueProprity(Node<T> temp, Node<T> tempNode) {
-		tempNode = tempNode.getNext();
-		if(tempNode.getNext()==null) {
-			tempNode.setNext(temp);
-			tempNode = null;
-		}
-		return tempNode;
-	}
-	
-	
 	public T pop() {
 		Node<T> tempNode = firstNode;
 		firstNode = firstNode.getNext();
 		return tempNode.getData();
 	}
 
-	public int size(){
+	public int size() {
 		int size = 0;
 		Node<T> actualNode = firstNode;
-		while(actualNode != null){
+		while (actualNode != null) {
 			size++;
 			actualNode = actualNode.getNext();
 		}
@@ -75,11 +78,19 @@ public class Queue<T> {
 	public boolean isEmpty() {
 		return firstNode == null;
 	}
-	
 
 	public Node<T> peek() {
 		return firstNode;
 	}
 
-	
+	public static void main(String[] args) {
+		Queue<String> queue = new Queue<>();
+		queue.push("P1", 1);
+		queue.push("P2", 5);
+		queue.push("P3", 3);
+		queue.push("P4", 4);
+		queue.push("P5", 2);
+		queue.show();
+	}
+
 }
